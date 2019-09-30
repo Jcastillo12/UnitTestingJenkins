edd94a0fb85c2d4a859465223088de44d512d567
Jemerick Castillo <jemerick1224@gmail.com>
2019-09-29 21:27:33 -0500

    Add project files.

---
+++ UnitTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UnitTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+        public static String createMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
